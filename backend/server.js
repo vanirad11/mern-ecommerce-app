d88589e55e671eac02bd27d1dded87109930e3fc
Vani R <vanirad11@gmail.com>
2026-02-07 13:20:32 +0530

    all files

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan'
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
+import cors from 'cors';
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+//configure env
+dotenv.config();
+
+//databae config
+connectDB();
+
+//rest object
+const app = express();
+
+// Increase the limit for JSON payloads to 10MB
+app.use(bodyParser.json({ limit: '10mb' }));
+
+// Increase the limit for URL-encoded payloads (form submissions) to 10MB
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
