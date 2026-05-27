957fd685a51386bef2d9a20f4a5fa913ca8d869d
Vani R <vanirad11@gmail.com>
2026-05-27 11:54:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ dotenv.config();
 //databae config
 connectDB();
 
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+
 //rest object
 const app = express();
 
