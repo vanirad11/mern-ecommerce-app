aca75d055fee0bc07f3236edcb8ab4c98fa4abe5
Vani R <vanirad11@gmail.com>
2026-09-22 18:10:46 +0530

    Update ProductDetails.js

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -18,7 +18,7 @@ const ProductDetails = () => {
   const getProduct = async () => {
     try {
       const { data } = await axios.get(
-        `/api/v1/product/get-product/${params.slug}`,
+        `${process.env.REACT_APP_API_URL}/api/v1/product/get-product/${params.slug}`,
       );
       setProduct(data?.product);
       getSimilarProduct(data?.product._id, data?.product.category._id);
