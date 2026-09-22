452a8a83c231533ee371b5f99dfb722c52139e91
Vani R <vanirad11@gmail.com>
2026-09-22 16:44:21 +0530

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -158,7 +158,7 @@ const HomePage = () => {
             {products?.map((p) => (
               <div className="card m-2" key={p._id}>
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+                  src={`${process.env.REACT_APP_API_URL}/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
                 />
